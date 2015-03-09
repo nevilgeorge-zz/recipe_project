61c7e5aee3b8a5103e6a64ce9e18121d24d4efb8
Pete Huang <peteh22@gmail.com>
2015-03-09 16:22:21 -0500

    low glycemic index, low sodium

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from Recipe import Recipe
+import csv
+
+gi_transform = {}
+sod_transform = {}
+
+def csv_setup():
+  with open('lowgi.csv', 'rbU') as lowgi:
+    reader = csv.reader(lowgi)
+    for r in reader:
+      gi_transform[r[0].lower()] = r[1].lower()
+  with open('lowsodium.csv', 'rbU') as lowsod:
+    reader = csv.reader(lowsod)
+    for r in reader:
+      sod_transform[r[0].lower()] = r[1].lower()
+
 
 def main():
-	rec = Recipe("http://allrecipes.com/Recipe/Delicious-Ham-and-Potato-Soup/Detail.aspx?soid=recs_recipe_1")
-	print rec.find_ingredients()
-	print rec.find_preparation()
-	print rec.find_descriptors()
+  rec = Recipe("http://allrecipes.com/Recipe/Raisin-Cake/Detail.aspx")
+  csv_setup()
+  print rec.find_ingredients()
+  print rec.transform_gi(gi_transform)
+  print rec.transform_sodium(sod_transform)
+  print rec.find_preparation()
+  print rec.find_descriptors()
 
 if __name__ == '__main__':
-	main()
+  main()
