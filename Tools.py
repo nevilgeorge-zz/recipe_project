b65504f0d78d85b384e294bf8eccd2966a678f0c
Frank Avino <frankavino@Franks-MacBook-Pro.local>
2015-03-09 19:36:38 -0500

    Finding and returning list of tools used in recipe's steps

---
+++ Tools.py
@@ -0,0 +1,41 @@
+# requester.py
+import requests, re
+from bs4 import BeautifulSoup
+import csv
+
+class Tools:
+
+	# constructor takes in a link of a recipe from allRecipes.com
+	def __init__(self, link):
+		# web URL of the allRecipes link we are using in this instance of the Recipe
+		self.link = link
+		#list of tools
+		self.tools = []
+		# GET the markup of the URL and store it as a member
+		r = requests.get(link)
+		self.markup = BeautifulSoup(r.text)
+
+	# find all tools used in directions, and return a list of all the tools (in order)
+	def find_tools(self):
+		elements = self.markup.select('.directions')
+		arr = []
+		for el in elements:
+		    arr.append(el.text)
+		self.tools=arr
+
+		all_tools = []
+
+		#build a list of all tools
+		with open('tools.csv') as inputfile:
+			results = list(csv.reader(inputfile))
+			for tool in results:
+				tooltext = ''.join(tool)
+				if tooltext in ''.join(self.tools):
+					if tooltext:
+						all_tools.append(tooltext)
+				#if tool in ''.join(self.tools):
+				#	all_tools.append(tooltext)
+				#	print 'success2'
+
+
+		return all_tools
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from Recipe import Recipe
-
+from Tools import Tools
 def main():
 	rec = Recipe("http://allrecipes.com/Recipe/Delicious-Ham-and-Potato-Soup/Detail.aspx?soid=recs_recipe_1")
+	tools = Tools("http://allrecipes.com/Recipe/Delicious-Ham-and-Potato-Soup/Detail.aspx?soid=recs_recipe_1")
 	print rec.find_ingredients()
 	print rec.find_preparation()
 	print rec.find_descriptors()
-
+	print tools.find_tools()
 if __name__ == '__main__':
 	main()
