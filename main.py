9f559274f4df8b94cb3e73713f0acc43983271a1
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-03-10 19:29:03 -0500

    create_json pretty much working

--- main.py
+++ main.py
@@ -19,27 +19,37 @@ def csv_setup():
 def create_json(rec):
   result = {}
   result["ingredients"] = []
-  result["primary cooking method"] = ""
+  names = rec.find_ingredient_names()
+  quantities = rec.find_quantity_values()
+  measurements = rec.find_measurements()
   preps = rec.find_preparation()
   descs = rec.find_descriptors()
-  for i in range(len(preps)):
-    curr_ing = {}
-    curr_ing["descriptor"] = descs[i]
-    curr_ing["preparator"] = preps[i]
-    result["ingredients"].append(curr_ing)
+  for i in range(len(names)):
+    current = {}
+    current["name"] = names[i]
+    current["quantity"] = quantities[i]
+    current["measurement"] = measurements[i]
+    current["descriptor"] = descs[i]
+    current["preparator"] = preps[i]
+    result["ingredients"].append(current)
 
+  result["primary cooking method"] = ""
+  # get tools
+  tool = Tools(rec.link)
+  result["cooking tools"] = tool.find_tools()
   return result
 
 
 def main():
   rec = Recipe("http://allrecipes.com/Recipe/Raisin-Cake/Detail.aspx")
-  tools = Tools("http://allrecipes.com/Recipe/Raisin-Cake/Detail.aspx")
+  tools = Tools(rec.link)
   csv_setup()
-  print len(rec.find_quantities())
-  print len(rec.find_quantity_values())
-  print len(rec.find_preparation())
-  print len(rec.find_descriptors())
-  print len(rec.find_measurements())
+  # print len(rec.find_quantities())
+  # print len(rec.find_quantity_values())
+  # print len(rec.find_preparation())
+  # print len(rec.find_descriptors())
+  # print len(rec.find_measurements())
+  print create_json(rec)
 
 if __name__ == '__main__':
   main()
